2021d0f44ec86446e062600d44e6870edcfa5526
ElianoGitHub <153697530+ElianoGitHub@users.noreply.github.com>
2023-12-28 16:20:25 -0700

    Changed ES6 to general in script.js

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-// Assuming you have the 'cocktails' array defined in cocktails.js
-
 // Function to generate buttons for each cocktail
 function generateButtons() {
     const drinkSelectionDiv = document.getElementById('drinkSelection');
     
-    cocktails.forEach(cocktail => {
-      const button = document.createElement('button');
-      button.innerText = `Spin for ${cocktail.name}`;
-      button.onclick = () => spinAndDisplay(cocktail);
-      drinkSelectionDiv.appendChild(button);
+    // Directly referencing the cocktails array from the window object
+    window.cocktails.forEach(cocktail => {
+        const button = document.createElement('button');
+        button.innerText = `Spin for ${cocktail.name}`;
+        button.onclick = () => spinAndDisplay(cocktail);
+        drinkSelectionDiv.appendChild(button);
     });
-  }
-  
-  // Function to randomly select a cocktail and display its details
-  function spinAndDisplay(selectedCocktail) {
+}
+
+// Function to randomly select a cocktail and display its details
+function spinAndDisplay(selectedCocktail) {
     // Clear any previous content from the recipeDisplayDiv
     const recipeDisplayDiv = document.getElementById('recipeDisplay');
     recipeDisplayDiv.innerHTML = '';
-  
+
     // Logic to spin the wheel can be added here. For simplicity, let's assume a random cocktail is chosen instantly.
-    const randomlySelectedCocktail = cocktails[Math.floor(Math.random() * cocktails.length)];
-  
+    const randomlySelectedCocktail = window.cocktails[Math.floor(Math.random() * window.cocktails.length)];
+
     // Display the selected cocktail's details
     displayCocktailDetails(randomlySelectedCocktail);
-  }
-  
-  // Function to display selected cocktail details
-  function displayCocktailDetails(cocktail) {
+}
+
+// Function to display selected cocktail details
+function displayCocktailDetails(cocktail) {
     const recipeDisplayDiv = document.getElementById('recipeDisplay');
     
     // Create elements to display cocktail details
@@ -38,18 +37,27 @@ function generateButtons() {
     // Display ingredients
     const ingredientsList = document.createElement('ul');
     cocktail.ingredients.forEach(ingredient => {
-      const listItem = document.createElement('li');
-      listItem.innerText = ingredient;
-      ingredientsList.appendChild(listItem);
+        const listItem = document.createElement('li');
+        listItem.innerText = ingredient;
+        ingredientsList.appendChild(listItem);
     });
     recipeDisplayDiv.appendChild(ingredientsList);
   
     // Display instructions
     const instructionsParagraph = document.createElement('p');
-    instructionsParagraph.innerText = `Instructions: ${cocktail.instructions}`;
+    instructionsParagraph.innerText = 'Instructions:';
     recipeDisplayDiv.appendChild(instructionsParagraph);
-  }
-  
-  // Call the function to generate buttons when the page loads
-  window.onload = generateButtons;
+
+    const instructionsList = document.createElement('ul');
+    cocktail.instructions.forEach(instruction => {
+        const listItem = document.createElement('li');
+        listItem.innerText = instruction;
+        instructionsList.appendChild(listItem);
+    });
+    recipeDisplayDiv.appendChild(instructionsList);
+}
+
+// Call the function to generate buttons when the page loads
+window.onload = generateButtons;
+
   
